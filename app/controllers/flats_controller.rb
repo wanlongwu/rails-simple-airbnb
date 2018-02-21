eb169820d5bc8fe1cea35707290a8924e69f0a57
Wanlong Wu <wumalone@gmail.com>
2018-02-21 16:43:02 +0900

    WIP

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,28 @@
+class FlatsController < ApplicationController
+  before_action :find_flat, only: [:show]
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+  end
+
+  private
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  [
+{
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+},
+{
+  name: 'Capsult hotel',
+  address: '9 hover lane ',
+  description: 'kitchen and a beautiful conservatory',
+  price_per_night: 750,
+  number_of_guests: 1
+},
+{
+  name: 'GUEST ROOMS',
+  address: '6 Gardens London W9 1DT',
+  description: 'Enjoy your stay in a fully renovated room, designed with traditional Japanese touches. Catch up with work at the desk using WiFi or relax while watching the 40-inch TV with DVD/Blue Ray player. All bathrooms feature a glass screen, upgraded lighting system and TOTO.',
+  price_per_night: 375,
+  number_of_guests: 2
+},
+{
+  name: 'SUITES',
+  address: '10 Clifton Gardens guangzhou W9 1DT',
+  description: 'Make an elegant suite your home away from home, with various options to suit your taste and purpose. Catch up with work at the desk using WiFi, or enjoy movies on the 40-inch TV and DVD/Blue Ray player. Large windows allow for natural daylight',
+  price_per_night: 7,
+  number_of_guests: 3
+},
+{
+  name: 'EXECUTIVE ROOMS',
+  address: '999 Clifton Gardens London W9 1DT',
+  description: 'Business is a pleasure in an elegant 30 sq. m./322 sq. ft. room offering Executive Lounge access with complimentary breakfast and evening cocktails. Catch up with work at the desk using WiFi, or enjoy movies on the large flat-screen LCD TV. The Hilton Serenity Bed gives you a sense of the utmost relaxation.',
+  price_per_night: 775,
+  number_of_guests: 4
+}
+]
+)
